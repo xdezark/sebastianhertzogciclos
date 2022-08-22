96bca44a0c241538e0d1054a86c8891a2bc7b449
xdezark <sebahertzogg@gmail.com>
2022-08-22 14:44:28 -0300

    dezark

---
+++ main.js
@@ -0,0 +1,9 @@
+let nombre= prompt("Ingresar usuario administrador");
+let nombresing= "";
+
+while(nombre!="Sebastian"){
+    nombresing += nombre +"\n";
+    nombre= prompt("Ingresar usuario administrador");
+}
+    console.log(nombresing);
+
